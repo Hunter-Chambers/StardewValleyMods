56f1a866dd4ff006b416d0b405dcaf7d92bd97f3
hunter-chambers <hrchambers.333@gmail.com>
2024-04-10 10:59:45 -0500

    Add project files.

---
+++ AutoGrabTruffles/AutoGrabTrufflesConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoGrabTruffles
+{
+    public sealed class AutoGrabTrufflesConfig
+    {
+        public bool ApplyGathererProfession { get; set; } = true;
+        public string WhoseGathererProfessionToUse { get; set; } = "owner";
+        public bool ApplyBotanistProfession { get; set; } = true;
+        public string WhoseBotanistProfessionToUse { get; set; } = "owner";
+        public bool GainExperience {  get; set; } = true;
+        public string WhoGainsExperience { get; set; } = "owner";
+        public bool UpdateGameStats { get; set; } = true;
+    }
+}
---
+++ FishingMod/Framework/FishingHelper.cs
@@ -0,0 +1,34 @@
+﻿using StardewValley;
+using StardewValley.Tools;
+
+namespace FishingMod.Framework
+{
+    internal class FishingHelper
+    {
+        private static bool CanHook(FishingRod fishingRod)
+        {
+            return fishingRod.isNibbling &&
+                   !fishingRod.hit &&
+                   !fishingRod.isReeling &&
+                   !fishingRod.pullingOutOfWater &&
+                   !fishingRod.fishCaught &&
+                   !fishingRod.showingTreasure;
+        }
+
+        public static void AutoHook(FishingRod fishingRod, bool doVibrate)
+        {
+            if (CanHook(fishingRod))
+            {
+                fishingRod.timePerBobberBob = 1f;
+                fishingRod.timeUntilFishingNibbleDone = FishingRod.maxTimeToNibble;
+                fishingRod.DoFunction(Game1.player.currentLocation, (int)fishingRod.bobber.X, (int)fishingRod.bobber.Y, 1, Game1.player);
+
+                if (doVibrate)
+                {
+                    Rumble.rumble(0.95f, 200f);
+                }
+            }
+        }
+
+    }
+}
