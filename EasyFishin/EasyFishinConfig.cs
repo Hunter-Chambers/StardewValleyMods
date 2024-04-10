ab030e7a17456be3fe36cddb6e7617a3948c575a
hunter-chambers <hrchambers.333@gmail.com>
2024-04-10 18:17:50 -0500

    EasyFishin v1.0.0

---
+++ EasyFishin/EasyFishinConfig.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+
+namespace EasyFishin
+{
+    /// <summary>The mod configuration.</summary>
+    internal class EasyFishinConfig
+    {
+        /// <summary>Whether the game should consider every catch to be perfectly executed, even if it wasn't.</summary>
+        public bool AlwaysPerfect { get; set; } = true;
+
+        /// <summary>Whether to always find treasure.</summary>
+        public bool AlwaysFindTreasure { get; set; } = false;
+
+        /// <summary>Whether to catch fish instantly.</summary>
+        public bool InstantCatchFish { get; set; } = true;
+
+        /// <summary>Whether to catch treasure instantly.</summary>
+        public bool InstantCatchTreasure { get; set; } = true;
+
+        /// <summary>Whether fishing tackles last forever.</summary>
+        public bool InfiniteTackle { get; set; } = true;
+
+        /// <summary>Whether fishing bait lasts forever.</summary>
+        public bool InfiniteBait { get; set; } = true;
+
+        /// <summary>Whether to automatically hook the fish without player interaction or not.</summary>
+        public bool AutoHook { get; set; } = true;
+
+        /// <summary>Whether to disable controller vibration or not.</summary>
+        public bool DisableVibration { get; set; } = false;
+    }
+}
